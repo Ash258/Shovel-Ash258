e2f16ebd94f26aac3ac607fed856bcdf1851873b
Jakub Čábera <cabera.jakub@gmail.com>
2018-06-18 20:50:02 +0200

    Get rid of split-paths

--- bin/formatjson.ps1
+++ bin/formatjson.ps1
@@ -1,4 +1,4 @@
-if(!$env:SCOOP_HOME) { $env:SCOOP_HOME = resolve-path (split-path (split-path (scoop which scoop))) }
+if(!$env:SCOOP_HOME) { $env:SCOOP_HOME = resolve-path (scoop prefix scoop) }
 $formatjson = "$env:SCOOP_HOME/bin/formatjson.ps1"
 $path = "$psscriptroot/.." # checks the parent dir
 iex -command "$formatjson -path $path $($args |% { "$_ " })"
