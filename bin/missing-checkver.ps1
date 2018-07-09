793a421c03427bfe36b93c43e073dd88ff20915d
Jakub Čábera <cabera.jakub@gmail.com>
2018-07-09 18:47:31 +0200

    TODO

--- bin/missing-checkver.ps1
+++ bin/missing-checkver.ps1
@@ -1,4 +1,4 @@
 if(!($env:SCOOP_HOME)) { $env:SCOOP_HOME = Resolve-Path (scoop prefix scoop) }
-$missing_checkver = "$env:SCOOP_HOME/bin/missing-checkver.ps1"
-$dir = "$psscriptroot/.."
+$missing_checkver = "$env:SCOOP_HOME\bin\missing-checkver.ps1"
+$dir = "$psscriptroot\.."
 iex -command "$missing_checkver -dir $dir $($args |% { "$_ " })"
