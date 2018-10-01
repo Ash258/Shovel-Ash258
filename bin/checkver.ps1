429b184beea8b5c63b2f72bff8011ef62d061649
Jakub Čábera <cabera.jakub@gmail.com>
2018-10-01 11:28:39 +0200

    Tweak checkver a bit

--- bin/checkver.ps1
+++ bin/checkver.ps1
@@ -3,10 +3,22 @@
 	Check version of given manifests.
 .DESCRIPTION
 	Check version of given manifests (If no manifest is present, all manifests in root of repo will be checked).
+	-s Parameter is set to true by default. (use -ns to show all)
 	Few control parameters could be used.
 	-u - Update given manifests
 	-f - Force update given manifests
-	-s - Show only outdated manifests
+	-ns - Show all (even up to date) manifests
+.PARAMETER Manifest
+	Manifest to check.
+	It could be List of manifests, specific manifest or string with placeholder.
+.PARAMETER Dir
+	Where to search for manifest.
+	Default to root of repository.
+.PARAMETER Rest
+	-ns - Show all (even up to date) manifests
+	-u - Update given manifests
+	-f - Force update given manifests
+		Usefull for hash updates
 .EXAMPLE
 	PS: BUCKETROOT $ .\bin\checkver.ps1
 	Check all manifests in root.
@@ -37,32 +49,34 @@
 
 	PS: BUCKETROOT $ .\bin\checkver.ps1 MANIFEST -f
 	Check manifest with name MANIFEST.json in root and update even when there is no new version.
-.PARAMETER Manifest
-	Manifest to check.
-	It could be List of manifests, specific manifest or string with placeholder.
-.PARAMETER Dir
-	Where to search for manifest.
-	Default to root of repository.
-.PARAMETER Rest
-	-s - Show only outdated manifests
-	-u - Update given manifests
-	-f - Force update given manifests
-		Usefull for hash updates
 #>
 param(
 	[Parameter(ValueFromPipeline = $true)]
-	[String[]]
-	$manifest = "*",
-	[String] $dir = "$PSScriptRoot\..",
+	[String[]] $Manifest = '*',
+	[String] $Dir = "$PSScriptRoot\..",
+	[Switch] $NoSkip,
 	[Parameter(ValueFromRemainingArguments = $true)]
-	[String] $rest = ""
+	[System.Collections.ArrayList] $Rest = @('-s')
 )
 
 begin {
-	$dir = Resolve-Path $dir
-	if (-not $env:SCOOP_HOME) { $env:SCOOP_HOME = Resolve-Path (scoop prefix scoop) }
+	$Dir = Resolve-Path $Dir
+	$Rest += '' # Have to be here for type convert when using single parameter
+	$Rest = $Rest | Select -Unique # Remove duplicated switches
+
+	if (-not $env:SCOOP_HOME) {
+		$env:SCOOP_HOME = Resolve-Path (scoop prefix scoop)
+	}
+	# Handle default -s parameter
+	# Don't skip if NoSkip is present
+	if ($NoSkip) {
+		$Rest.Remove('-s')
+	} else {
+		# Append -s if there is none
+		if ($Rest -cnotcontains '-s') { $Rest += '-s' }
+	}
 }
 
 process {
-	foreach ($man in $manifest) { Invoke-Expression -Command "$env:SCOOP_HOME\bin\checkver.ps1 -dir ""$dir"" -app ""$man"" $rest" }
+	foreach ($man in $Manifest) { Invoke-Expression -Command "$env:SCOOP_HOME\bin\checkver.ps1 -App ""$man"" -Dir ""$Dir"" $($Rest -join ' ')" }
 }
