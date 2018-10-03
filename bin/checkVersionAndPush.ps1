6535eec3039e63b2ad146711974a46553c70e702
Jakub Čábera <cabera.jakub@gmail.com>
2018-10-04 00:18:13 +0200

    Add new bin

---
+++ bin/checkVersionAndPush.ps1
@@ -0,0 +1,29 @@
+<#
+.SYNOPSIS
+	Update manifest, commit and push.
+.PARAMETER Manifest
+	Full Path to manifest. (vscode ${file})
+.PARAMETER Force
+	If present, -f will be used instead of -u.
+#>
+param(
+	[String] $Manifest,
+	[Switch] $Force
+)
+
+$Manifest = Resolve-Path $Manifest
+$folder = Split-Path $Manifest -Parent
+$file = Split-Path $Manifest -Leaf
+$noExt = $file.Split('.')[0]
+if ($Force) { $arg = '-f' } else { $arg = '-u'}
+
+Invoke-Expression -Command "$PSScriptRoot\checkver.ps1 '$noExt' '$folder' $arg"
+
+$updated = (git status -s)
+$version = (Get-Content "$Manifest" | ConvertFrom-Json).version
+
+if ($updated -match "$noExt") {
+	Write-Host 'ANo'
+	git commit -m "${noExt}: Bump to $version" -o $file
+	git push
+}
