74e4510195726f7ffb0a283f6cc4266d3854f188
Jakub Čábera <cabera.jakub@gmail.com>
2018-10-05 19:15:49 +0200

    Add missing file

---
+++ bin/createHooks.ps1
@@ -0,0 +1,17 @@
+$HOOKS_FOLD = Resolve-Path "$PSScriptRoot\..\.git\hooks"
+
+# Exit when hooks folder does not exists for some reason
+if (!(Test-Path $HOOKS_FOLD -Type Container)){
+	Write-Host 'Folder' $HOOKS_FOLD 'Does not Exists. Exiting' -ForegroundColor Red
+	exit 1
+}
+
+$HOOKS = @{
+	'PRE-COMMIT' = @(
+		'#!/bin/sh',
+		'',
+		"powershell.exe -File '$PSScriptRoot\test.ps1'"
+	);
+}
+
+$HOOKS.Keys | ForEach-Object { Set-Content "$HOOKS_FOLD\$_" ($HOOKS.Item($_) -join "`n") -Encoding Ascii }
