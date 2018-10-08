9d536b47b77fcb6a7feea84d919d8d052c7a8e39
Jakub Čábera <cabera.jakub@gmail.com>
2018-10-08 16:45:51 +0200

    Lint tests and add install test

--- Scoop-Bucket.Tests.ps1
+++ Scoop-Bucket.Tests.ps1
@@ -3,188 +3,197 @@
 . "$env:SCOOP_HOME\lib\unix.ps1"
 . "$env:SCOOP_HOME\test\Scoop-TestLib.ps1"
 
-$repo_dir = (Get-Item $MyInvocation.MyCommand.Path).directory.FullName
+$repoDirectory = (Get-Item $MyInvocation.MyCommand.Path).Directory.FullName
 
-$repo_files = @(Get-ChildItem $repo_dir -file -recurse)
+$repoFiles = @(Get-ChildItem $repoDirectory -File -Recurse)
 
-$project_file_exclusions = @(
-    $([regex]::Escape($repo_dir) + '(\\|/).git(\\|/).*$'),
-    $([regex]::Escape($repo_dir) + '(\\|/)bin(\\|/).*$'),
-    '.sublime-workspace$',
-    '.DS_Store$',
-    'supporting(\\|/)validator(\\|/)packages(\\|/)*'
+$projectFileExclusions = @(
+	$([Regex]::Escape($repoDirectory) + '(\\|/).git(\\|/).*$'),
+	$([Regex]::Escape($repoDirectory) + '(\\|/)bin(\\|/).*$'),
+	'.sublime-workspace$',
+	'.DS_Store$',
+	'supporting(\\|/)validator(\\|/)packages(\\|/)*'
 )
 
-describe 'Style constraints for non-binary project files' {
+Describe 'Style constraints for non-binary project files' {
 
-    $files = @(
-        # gather all files except '*.exe', '*.zip', or any .git repository files
-        $repo_files |
-            where-object { $_.fullname -inotmatch $($project_file_exclusions -join '|') } |
-            where-object { $_.fullname -inotmatch '(.exe|.zip|.dll)$' }
-    )
+	$files = @(
+		# gather all files except '*.exe', '*.zip', or any .git repository files
+		$repoFiles |
+			Where-Object { $_.FullName -inotmatch $($projectFileExclusions -join '|') } |
+			Where-Object { $_.FullName -inotmatch '(.exe|.zip|.dll)$' }
+	)
 
-    $files_exist = ($files.Count -gt 0)
+	$filesExist = ($files.Count -gt 0)
 
-    it $('non-binary project files exist ({0} found)' -f $files.Count) -skip:$(-not $files_exist) {
-        if (-not ($files.Count -gt 0)) {
-            throw "No non-binary project were found"
-        }
-    }
+	It "Non-binary project files exist {$($files.Count)} found)" -Skip:$(-not $filesExist) {
+		if (-not ($files.Count -gt 0)) { throw 'No non-binary project were found' }
+	}
 
-    it 'files do not contain leading utf-8 BOM' -skip:$(-not $files_exist) {
-        # utf-8 BOM == 0xEF 0xBB 0xBF
-        # see http://www.powershellmagazine.com/2012/12/17/pscxtip-how-to-determine-the-byte-order-mark-of-a-text-file @@ https://archive.is/RgT42
-        # ref: http://poshcode.org/2153 @@ https://archive.is/sGnnu
-        $badFiles = @(
-            foreach ($file in $files) {
-                $content = ([char[]](Get-Content $file.FullName -encoding byte -totalcount 3) -join '')
-                if ([regex]::match($content, '(?ms)^\xEF\xBB\xBF').success) {
-                    $file.FullName
-                }
-            }
-        )
+	It 'Files do not contain leading utf-8 BOM' -Skip:$(-not $filesExist) {
+		# utf-8 BOM == 0xEF 0xBB 0xBF
+		# see http://www.powershellmagazine.com/2012/12/17/pscxtip-how-to-determine-the-byte-order-mark-of-a-text-file @@ https://archive.is/RgT42
+		# ref: http://poshcode.org/2153 @@ https://archive.is/sGnnu
+		$badFiles = @(
+			foreach ($file in $files) {
+				$content = ([Char[]](Get-Content $file.FullName -Encoding Byte -TotalCount 3) -join '')
+				if ([Regex]::Match($content, '(?ms)^\xEF\xBB\xBF').Success) {
+					$file.FullName
+				}
+			}
+		)
 
-        if ($badFiles.Count -gt 0) {
-            throw "The following files have utf-8 BOM: `r`n`r`n$($badFiles -join "`r`n")"
-        }
-    }
+		if ($badFiles.Count -gt 0) {
+			throw "The following files have utf-8 BOM: `r`n`r`n$($badFiles -join "`r`n")"
+		}
+	}
 
-    it 'files end with a newline' -skip:$(-not $files_exist) {
-        $badFiles = @(
-            foreach ($file in $files) {
-                $string = [System.IO.File]::ReadAllText($file.FullName)
-                if ($string.Length -gt 0 -and $string[-1] -ne "`n") {
-                    $file.FullName
-                }
-            }
-        )
+	It 'Files end with a newline' -Skip:$(-not $filesExist) {
+		$badFiles = @(
+			foreach ($file in $files) {
+				$string = [System.IO.File]::ReadAllText($file.FullName)
+				if ($string.Length -gt 0 -and $string[-1] -ne "`n") {
+					$file.FullName
+				}
+			}
+		)
 
-        if ($badFiles.Count -gt 0) {
-            throw "The following files do not end with a newline: `r`n`r`n$($badFiles -join "`r`n")"
-        }
-    }
+		if ($badFiles.Count -gt 0) {
+			throw "The following files do not end with a newline: `r`n`r`n$($badFiles -join "`r`n")"
+		}
+	}
 
-    it 'file newlines are CRLF' -skip:$(-not $files_exist) {
-        $badFiles = @(
-            foreach ($file in $files) {
-                $content = Get-Content -raw $file.FullName
-                if (!$content) {
-                    throw "File contents are null: $($file.FullName)"
-                }
-                $lines = [regex]::split($content, '\r\n')
-                $lineCount = $lines.Count
+	It 'File newlines are CRLF' -Skip:$(-not $filesExist) {
+		$badFiles = @(
+			foreach ($file in $files) {
+				$content = Get-Content -Raw $file.FullName
+				if (!$content) {
+					throw "File contents are null: $($file.FullName)"
+				}
+				$lines = [Regex]::Split($content, '\r\n')
+				$lineCount = $lines.Count
 
-                for ($i = 0; $i -lt $lineCount; $i++) {
-                    if ( [regex]::match($lines[$i], '\r|\n').success ) {
-                        $file.FullName
-                        break
-                    }
-                }
-            }
-        )
+				for ($i = 0; $i -lt $lineCount; $i++) {
+					if ( [Regex]::Match($lines[$i], '\r|\n').Success ) {
+						$file.FullName
+						break
+					}
+				}
+			}
+		)
 
-        if ($badFiles.Count -gt 0) {
-            throw "The following files have non-CRLF line endings: `r`n`r`n$($badFiles -join "`r`n")"
-        }
-    }
+		if ($badFiles.Count -gt 0) {
+			throw "The following files have non-CRLF line endings: `r`n`r`n$($badFiles -join "`r`n")"
+		}
+	}
 
-    it 'files have no lines containing trailing whitespace' -skip:$(-not $files_exist) {
-        $badLines = @(
-            foreach ($file in $files) {
-                $lines = [System.IO.File]::ReadAllLines($file.FullName)
-                $lineCount = $lines.Count
+	It 'Files have no lines containing trailing whitespace' -Skip:$(-not $filesExist) {
+		$badLines = @(
+			foreach ($file in $files) {
+				$lines = [System.IO.File]::ReadAllLines($file.FullName)
+				$lineCount = $lines.Count
 
-                for ($i = 0; $i -lt $lineCount; $i++) {
-                    if ($lines[$i] -match '\s+$') {
-                        'File: {0}, Line: {1}' -f $file.FullName, ($i + 1)
-                    }
-                }
-            }
-        )
+				for ($i = 0; $i -lt $lineCount; $i++) {
+					if ($lines[$i] -match '\s+$') {
+						"File: {$($file.FullName)}, Line: {$($i +1)}"
+					}
+				}
+			}
+		)
 
-        if ($badLines.Count -gt 0) {
-            throw "The following $($badLines.Count) lines contain trailing whitespace: `r`n`r`n$($badLines -join "`r`n")"
-        }
-    }
+		if ($badLines.Count -gt 0) {
+			throw "The following $($badLines.Count) lines contain trailing whitespace: `r`n`r`n$($badLines -join "`r`n")"
+		}
+	}
 
-    it 'any leading whitespace consists only of spaces (excepting makefiles)' -skip:$(-not $files_exist) {
-        $badLines = @(
-            foreach ($file in $files) {
-                if ($file.fullname -inotmatch '(^|.)makefile$') {
-                    $lines = [System.IO.File]::ReadAllLines($file.FullName)
-                    $lineCount = $lines.Count
+	It 'Any leading whitespace consists only of spaces (excepting powershell scripts)' -Skip:$(-not $filesExist) {
+		$badLines = @(
+			foreach ($file in $files) {
+				if ($file.FullName -inotmatch '.ps1$') {
+					$lines = [System.IO.File]::ReadAllLines($file.FullName)
+					$lineCount = $lines.Count
 
-                    for ($i = 0; $i -lt $lineCount; $i++) {
-                        if ($lines[$i] -notmatch '^[ ]*(\S|$)') {
-                            'File: {0}, Line: {1}' -f $file.FullName, ($i + 1)
-                        }
-                    }
-                }
-            }
-        )
+					for ($i = 0; $i -lt $lineCount; $i++) {
+						if ($lines[$i] -notmatch '^[ ]*(\S|$)') {
+							"File: {$($file.FullName)}, Line: {$($i +1)}"
+						}
+					}
+				}
+			}
+		)
 
-        if ($badLines.Count -gt 0) {
-            throw "The following $($badLines.Count) lines contain TABs within leading whitespace: `r`n`r`n$($badLines -join "`r`n")"
-        }
-    }
-
+		if ($badLines.Count -gt 0) {
+			throw "The following $($badLines.Count) lines contain TABs within leading whitespace: `r`n`r`n$($badLines -join "`r`n")"
+		}
+	}
 }
 
-describe "manifest-validation" {
-    beforeall {
-        $working_dir = setup_working "manifest"
-        $schema = "$env:SCOOP_HOME/schema.json"
-        Add-Type -Path "$env:SCOOP_HOME\supporting\validator\bin\Newtonsoft.Json.dll"
-        Add-Type -Path "$env:SCOOP_HOME\supporting\validator\bin\Newtonsoft.Json.Schema.dll"
-        Add-Type -Path "$env:SCOOP_HOME\supporting\validator\bin\Scoop.Validator.dll"
-    }
+Describe 'Manifest-validation' {
+	BeforeAll {
+		$workingDirectory = setup_working 'manifest'
+		$schema = "$env:SCOOP_HOME/schema.json"
+		Add-Type -Path "$env:SCOOP_HOME\supporting\validator\bin\Newtonsoft.Json.dll"
+		Add-Type -Path "$env:SCOOP_HOME\supporting\validator\bin\Newtonsoft.Json.Schema.dll"
+		Add-Type -Path "$env:SCOOP_HOME\supporting\validator\bin\Scoop.Validator.dll"
+	}
 
-    it "Scoop.Validator is available" {
-        ([System.Management.Automation.PSTypeName]'Scoop.Validator').Type | should be 'Scoop.Validator'
-    }
+	It 'Scoop.Validator is available' {
+		([System.Management.Automation.PSTypeName] 'Scoop.Validator').Type | Should Be 'Scoop.Validator'
+	}
 
-    context "manifest validates against the schema" {
-        beforeall {
-            $bucketdir = "$psscriptroot"
-            $manifest_files = Get-ChildItem $bucketdir *.json
-            $validator = new-object Scoop.Validator($schema, $true)
-        }
+	Context 'Manifest validates against the schema' {
+		BeforeAll {
+			$bucketDirectory = "$PSScriptRoot"
+			$manfiestFiles = Get-ChildItem $bucketDirectory '*.json'
+			$validator = New-Object Scoop.Validator($schema, $true)
+		}
 
-        $global:quota_exceeded = $false
+		$global:quota_exceeded = $false
 
-        $manifest_files | ForEach-Object {
-            it "$_" {
-                $file = $_ # exception handling may overwrite $_
+		$manfiestFiles | ForEach-Object {
+			It "$_" {
+				$file = $_ # exception handling may overwrite $_
 
-                if (!($global:quota_exceeded)) {
-                    try {
-                        $validator.Validate($file.fullname)
+				if (!($global:quota_exceeded)) {
+					try {
+						$validator.Validate($file.FullName)
 
-                        if ($validator.Errors.Count -gt 0) {
-                            write-host -f yellow $validator.ErrorsAsString
-                        }
-                        $validator.Errors.Count | should be 0
-                    }
-                    catch {
-                        if ($_.exception.message -like '*The free-quota limit of 1000 schema validations per hour has been reached.*') {
-                            $global:quota_exceeded = $true
-                            write-host -f darkyellow 'Schema validation limit exceeded. Will skip further validations.'
-                        }
-                        else {
-                            throw
-                        }
-                    }
-                }
+						if ($validator.Errors.Count -gt 0) {
+							Write-Host $validator.ErrorsAsString -ForegroundColor Yellow
+						}
+						$validator.Errors.Count | Should Be 0
+					} catch {
+						if ($_.Exception.Message -like '*The free-quota limit of 1000 schema validations per hour has been reached.*') {
+							$global:quota_exceeded = $true
+							Write-Host 'Schema validation limit exceeded. Will skip further validations.' -ForegroundColor DarkYellow
+						} else {
+							throw
+						}
+					}
+				}
 
-                $manifest = parse_json $file.fullname
-                $url = arch_specific "url" $manifest "32bit"
-                $url64 = arch_specific "url" $manifest "64bit"
-                if (!$url) {
-                    $url = $url64
-                }
-                $url | should not benullorempty
-            }
-        }
-    }
+				$manifest = parse_json $file.FullName
+				$url = arch_specific 'url' $manifest '32bit'
+				$url64 = arch_specific 'url' $manifest '64bit'
+				if (!$url) {
+					$url = $url64
+				}
+				$url | Should Not BeNullOrEmpty
+			}
+		}
+	}
+}
+
+Describe 'Test installation of added manifests' {
+	if ($env:CI -eq $true) {
+		$commit = if ($env:APPVEYOR_PULL_REQUEST_HEAD_COMMIT) { $env:APPVEYOR_PULL_REQUEST_HEAD_COMMIT } else { $env:APPVEYOR_REPO_COMMIT }
+		$commitMessage = "$env:APPVEYOR_REPO_COMMIT_MESSAGE $env:APPVEYOR_REPO_COMMIT_MESSAGE_EXTENDED".TrimEnd()
+		$changedFiles = Get-GitChangedFile -Include '*.json' -Commit $commit
+		$changedFiles | ForEach-Object {
+			It "Intall {$_}" {
+				scoop install "./$_"
+				$LASTEXITCODE | Should Be 0
+			}
+		}
+	}
 }
