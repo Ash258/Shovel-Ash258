3b4dde1894952cb66f69766a2e6c394a47cefa91
Jakub Čábera <cabera.jakub@gmail.com>
2020-12-03 18:28:50 +0100

    idiot v2

--- Bucket.Tests.ps1
+++ Bucket.Tests.ps1
@@ -60,8 +60,8 @@ Describe 'Changed manifests installation' {
         return
     }
 
-    New-Item "$env:SCOOP_HOME\shims" -ItemType Directory | Out-Null
-    Set-Content "$env:SCOOP_HOME\shims\scoop.ps1" @'
+    New-Item "$env:SCOOP\shims" -ItemType Directory | Out-Null
+    Set-Content "$env:SCOOP\shims\scoop.ps1" @'
 $path = Join-Path "$PSScriptRoot" "..\apps\scoop\current\bin\scoop.ps1"
 if ($MyInvocation.ExpectingInput) { $input | & $path @args } else { & $path @args }
 exit $LASTEXITCODE
