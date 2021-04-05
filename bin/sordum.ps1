ebe5b3531e9a2e4e193475599bcf7657f2086042
Jakub Čábera <cabera.jakub@gmail.com>
2021-04-05 03:50:29 -0700

    todo bumps [shovel skip]

--- bin/sordum.ps1
+++ bin/sordum.ps1
@@ -2,5 +2,5 @@ Join-Path $PSScriptRoot '..\bucket' -Resolve | Get-ChildItem | Where-Object {
     $dsc = (((Get-Content $_.FullName) -like '*description*:*') -split ':')[-1]
     return ($dsc -like '*sordum*') -or ($dsc -like 'Kaspersky*') -or ($dsc -like '*mitec*')
 } | ForEach-Object {
-    & "$PSScriptRoot\checkver.ps1" $_.BaseName -Force
+    shovel utils checkver $_.FullName --additional-options -force
 }
