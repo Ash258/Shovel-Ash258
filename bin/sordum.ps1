1cc8e3065bfdacc9e7e9d795d7c300504e11facb
Jakub Čábera <cabera.jakub@gmail.com>
2021-04-21 18:22:41 +0200

    TotalCommander-beta: Update to version 10.00b6
    - Closes #267
    - Closes #268
    - Closes #269
    - Closes #270

--- bin/sordum.ps1
+++ bin/sordum.ps1
@@ -1,6 +1,9 @@
+param([Switch] $UseCache)
+
 Join-Path $PSScriptRoot '..\bucket' -Resolve | Get-ChildItem | Where-Object {
     $dsc = (((Get-Content $_.FullName) -like '*description*:*') -split ':')[-1]
     return ($dsc -like '*sordum*') -or ($dsc -like 'Kaspersky*') -or ($dsc -like '*mitec*')
 } | ForEach-Object {
+    if (!$UseCache) { shovel cache rm $_.BaseName }
     shovel utils checkver $_.FullName --additional-options -force
 }
