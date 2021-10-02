1c359f8af4eaf16ec40c2633678eaac141357a8f
Jakub Čábera <cabera.jakub@gmail.com>
2021-10-02 04:19:20 -0700

    [shovel skip]

--- bin/sordum.ps1
+++ bin/sordum.ps1
@@ -4,10 +4,11 @@ $items = Join-Path $PSScriptRoot '..\bucket' -Resolve | Get-ChildItem -File | Wh
     $dsc = (((Get-Content $_.FullName) -like '*description*:*') -split ':')[-1]
     return ($dsc -like '*sordum*') -or ($dsc -like 'Kaspersky*') -or ($dsc -like '*mitec*')
 }
+$items += Get-ChildItem "$PSScriptRoot\..\bucket\*" -Include 'PotPlayer*', 'QuickCPU*'
 
 if (!$UseCache) {
     $i = $items.Basename
-    Write-Host 'Removing previous downloads'
+    Write-Host 'Removing previous downloads' -ForegroundColor 'Green'
     shovel cache rm @i
 }
 
